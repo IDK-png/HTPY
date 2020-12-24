1698bdc69853d33f772311768d045abeeffee05c
IDK-png <59571985+IDK-png@users.noreply.github.com>
2020-12-24 21:57:33 +0200

    Не важно
    Я и-за теста другого проекта(не спрашиваете какого) убрал все файлы н**уй и я их вернул

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,2 @@
 #import checkscript as check
 #import stringfuncs as echo
-#class htpy():
- #   def htmlcheck(self):
- #      return check.files()
-#    def echo(self,line):
-#        return echo.base(line)
-#tests = htpy().echo('echo("d") -t')
-#print(tests)
